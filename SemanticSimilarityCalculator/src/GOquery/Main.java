482ec6b1a0f9548561b07018b68b370716b4ef12
zbvd935 <zbvd935@cim-ts-node-01.cc.rhul.local>
2017-11-01 10:28:39 +0000

    project created on github, first upload to repository

---
+++ SemanticSimilarityCalculator/src/GOquery/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package GOquery;
+
+/**
+ * @author zbvd935
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
