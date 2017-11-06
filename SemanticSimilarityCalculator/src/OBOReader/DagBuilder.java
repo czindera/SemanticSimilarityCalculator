9864193cf648a6b4fb770c0c7ce782f7c43cea34
Attila Czinder <Attila.Czinder.2015@live.rhul.ac.uk>
2017-11-06 22:48:13 +0000

    First attempt to read the file and create the nodes(Vertices) only

--- SemanticSimilarityCalculator/src/OBOReader/DagBuilder.java
+++ SemanticSimilarityCalculator/src/OBOReader/DagBuilder.java
@@ -30,56 +30,78 @@ public class DagBuilder {
 	 */
 	private String next() throws IOException {
 		if(buffer!=null) {
-		String s=buffer;
-		buffer=null;
-		return s;
+			String s=buffer;
+			buffer=null;
+			return s;
 		}
-	return in.readLine();
+		return in.readLine();
+	}
+	
+	/**
+	 * This method is to trim the comments from a line
+	 * @param s
+	 * @return
+	 */
+	private String nocomment(String s) {
+		int excLocation=s.indexOf('!');
+		if(excLocation!=-1) s=s.substring(0,excLocation);
+		return s.trim();
 	}
 	
 	/**
 	 * parse one Term
 	 * @throws IOException
 	 */
-	private void parseTerm() throws IOException
-	{
-	Term t=null;
-	String line;
-	while((line=next())!=null)
-		{
-		if(line.startsWith("["))
-			{
-			this.buffer=line;
-			break;
-			}
-		int colon=line.indexOf(':');
-		if(colon==-1) continue;
-		if(line.startsWith("id:") && t==null)
-			{
-			t=getTermById(line.substring(colon+1).trim(),true);
-			continue;
-			}
-		if(t==null) continue;
-		if(line.startsWith("name:"))
-			{
-			t.name=nocomment(line.substring(colon+1));
-			continue;
-			}
-		else if(line.startsWith("def:"))
-			{
-			t.def=nocomment(line.substring(colon+1));
-			continue;
-			}
-		else if(line.startsWith("is_a:"))
-			{
-			String rel=nocomment(line.substring(colon+1));
-			t.is_a.add(rel);
-			Term parent=getTermById(rel,true);
-			parent.children.add(t.id);
-			continue;
-			}
+	private void parseTerm() throws IOException {
+		String line;
+		Term newTerm = new Term();
+		while((line=next())!=null) {
+			if(line.startsWith("["))
+				{
+				this.buffer=line;
+				break;
+				}
+			int colon=line.indexOf(':');
+			if(colon==-1) continue;
+			if(line.startsWith("id:"))
+				{
+				newTerm.id = line.substring(colon+1).trim();
+				continue;
+				}
+			//if(t==null) continue;
+			if(line.startsWith("name:"))
+				{
+				newTerm.name=nocomment(line.substring(colon+1));
+				continue;
+				}
+			if(line.startsWith("namespace:"))
+				{
+				newTerm.namespace=nocomment(line.substring(colon+1));
+				continue;
+				}
+			else if(line.startsWith("def:"))
+				{
+				newTerm.def=nocomment(line.substring(colon+1));
+				continue;
+				}
+			dag.addVertex(newTerm);
 		}
 	}
+	
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	/*private void createEdges() throws IOException {
+		if(line.startsWith("is_a:"))
+		{
+		String rel=nocomment(line.substring(colon+1));
+		t.is_a.add(rel);
+		Term parent=getTermById(rel,true);
+		parent.children.add(t.id);
+		continue;
+		}
+	}*/
 	
 	/**
 	 * parse the whole file
